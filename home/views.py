77cace1cab6970a34cf3bfaa2e5158fe3ebb8560
Mohdsaqib <mohdsaqibshayar@gmail.com>
2023-11-23 18:37:10 +0530

    connecting

--- home/views.py
+++ home/views.py
@@ -1,22 +1,27 @@
 from django.shortcuts import render, redirect
 
 from .models import Task
+from django_htmx.http import HttpResponseClientRefresh
 # Create your views here.
 def index(request):
     context ={
          'task_list': Task.objects.all()
     }
     return render(request,'index.html', context)
+
 def add_task(request):
-        if request.method =='POST':
-             task= request.POST.get('task')
-             if len(task)>=2:
-                  task.objects.create(title=task)
-        redirect('home')
-        context={}
-        return render(request,'todo_list.html', context)
+    if request.method =='POST':
+        task= request.POST.get('task')
+        if len(task)>=2:
+            Task.objects.create(title=task)  
+    return HttpResponseClientRefresh()
 def update_task(request,id):
-    context={}
-    return render(request,'todo_list.html', context)
+    print ('checkbox clicked')
+    print('the id is', id)
+    Task.objects.filter(id=id).update(is_completed=True)#filter and update
+    return HttpResponseClientRefresh()
+
 def delete_task(request, id):
-     pass
+    out = Task.objects.filter(id=id).delete()
+    print(out)
+    return HttpResponseClientRefresh()
